7bedc5715d67354d45ff6392292673af8eff3171
Andy Armstrong <andyarmstro@gmail.com>
2016-11-04 19:17:59 -0700

    ER-41: updated templates

--- src/erdiko/shopify/views/shopify/index.php
+++ src/erdiko/shopify/views/shopify/index.php
@@ -1,8 +1,19 @@
-
-<ul>
-	<li><a href="/shop/metafields">Metafields</a></li>
-    <li><a href="/shop/products">Products</a></li>
-	<li><a href="/shop/customers">Customers</a></li>
-    <li><a href="/shop/orders">Orders</a></li>
-    <li><a href="/shop/news">News Blog</a></li>
+<div class="container">
+    <div class="page-header" id="banner">
+        <div class="row">
+          <div class="col-lg-8 col-md-7 col-sm-6">
+            <h1>Welcome to Erdiko Shopify</h1>
+          </div>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <ul class="list-group">
+            <li class="list-group-item"><a href="/shop/metafields">Metafields</a></li>
+            <li class="list-group-item"><a href="/shop/products">Products</a></li>
+            <li class="list-group-item"><a href="/shop/customers">Customers</a></li>
+            <li class="list-group-item"><a href="/shop/orders">Orders</a></li>
 </ul>
+      </div>
+    </div>
+</div>
---
+++ src/erdiko/shopify/views/shopify/json.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="page-header" id="banner">
+        <div class="row">
+          <div class="col-lg-8 col-md-7 col-sm-6">
+            <h1>Title</h1>
+          </div>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+
+        <code> 
+            <?php echo var_dump($data); ?>
+        </code>
+
+      </div>
+    </div>
+</div>
