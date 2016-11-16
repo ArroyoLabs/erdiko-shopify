8d0bb12d2a29c4a5e7f14c19937bd07ff57d2101
Andy Armstrong <andyarmstro@gmail.com>
2016-11-15 18:25:19 -0800

    ER-72: removed unneeded view files ; updated view for product meta-data

--- src/erdiko/shopify/views/shopify/grid/storeInfoFormInput.php
+++
@@ -1,6 +0,0 @@
-<tr>
-	
-	<td><?php echo $data['details']['key'] ?>:</td>
-	<td><input type="text" name="<?php echo $data['details']['key']; ?>" value="<?php echo $data['details']['value'];?>"/></td>
-</tr>
-			
